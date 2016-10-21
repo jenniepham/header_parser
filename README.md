a19c1d4b4819aebcc61b95b666681414bf6c3723
Jennie <jenniepham0221@gmail.com>
2016-10-21 21:12:39 +0000

    challenge complete

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+Get IP, Language, OS
+
+Example output:
+
+"IP Address" : "xxx.xxx.xxx.xxx"
+"Language" : "en-US"
+"Operating System" : "X11; Linux x86_64" 
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    
+    var ip = request.headers['x-forwarded-for'];
+    var language = request.headers["accept-language"].split(',')[0];
+    var os = request.headers['user-agent'].split(') ')[0].split(' (')[1];
+    
+    var result = "IP Address: " + ip + "\nLanguage:" + language + "\nOperating system: " + os;
+    
+
+    response.end(result);
+    console.log(request.headers);
+    
+});
+
+app.listen(8080, function(){
+    
+   console.log("Listening on port 8080"); 
+   
+});
